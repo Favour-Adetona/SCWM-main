9c5726caa65509bdbb33d1fe93d09d5a86dc20da
Favour-Adetona <adetonsfave@gmail.com>
2024-12-11 18:23:32 +0100

    Add files via upload

---
+++ filemanip.h
@@ -0,0 +1,10 @@
+#ifndef FILEMANIP_H
+#define FILEMANIP_H
+
+
+
+void checkCommand(int argc, char **argv);
+void writeAndReadFile(const char *filename);
+void WriteLinesToFile(const char *filename);
+
+#endif // FILEMANIP_H
